3f957405381b94c6001e2ac81dd56a810608cbdf
douglasPS <tridops@gmail.com>
2019-10-30 18:05:03 -0300

    right

--- app/Http/Controllers/turingController.php
+++ app/Http/Controllers/turingController.php
@@ -15,7 +15,7 @@ class turingController extends Controller
             $transicao[$i] = explode('-',$flag);
             $transicao[$i][0] = explode(',',$transicao[$i][0]);
             $transicao[$i][1] = explode(',',$transicao[$i][1]);
-        }        
+        }
 
         // Entrada
         $entrada = str_split($request->entrada);
@@ -38,14 +38,19 @@ class turingController extends Controller
                 }
                 // Pega o primeiro estado
                 $flagEstado = $transicao[$i][1][0];
-            } else {
-                foreach ($transicao as $key => $value) {
-                    if($value[0][0] == $flagEstado && $value[0][1] == $entrada[$i-1]){                        
-                        $fita[$i] = $transicao[$i][1][1];
+            } else {                
+                foreach ($transicao as $key => $value) { 
+                    // Testes
+                    // if($i == 4 && $value[0][0] == $flagEstado){ 
+                    //     dd($value, $flagEstado, $value[0][1], $entrada[$i-1]);
+                    // }
+                    if($value[0][0] == $flagEstado && $value[0][1] == $entrada[$i-1]){ 
+                        
+                        $fita[$i] = $value[1][1];
                         $flagEstado = $value[1][0];
-                    }
+                    }                     
                 }
-            }            
+            }
         }
         dd($fita);
         return redirect("/")->with('success', $fita);
